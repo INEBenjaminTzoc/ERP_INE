f08549d7585bf719193725fc66dfd23d5e71e4b0
Benjamin Tzoc <jptzoc@ine.gob.gt>
2025-03-20 16:00:15 -0600

    Primer commit ERP_INE

---
+++ src/app/pages/auth/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { AppFloatingConfigurator } from "../../../layout/component/app.floatingconfigurator";
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { Router, RouterModule } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { CheckboxModule } from 'primeng/checkbox';
+import { RippleModule } from 'primeng/ripple';
+
+const modules = [
+  AppFloatingConfigurator, 
+  FormsModule, 
+  PasswordModule, 
+  ButtonModule, 
+  InputTextModule, 
+  CheckboxModule,
+  RouterModule,
+  RippleModule
+]
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: modules,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  checked: boolean = false;
+
+  constructor (
+    private router: Router,
+    private formBuild: FormBuilder
+  ) {
+    this.formLogin = formBuild.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  formLogin: FormGroup;
+}
